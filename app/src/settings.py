f2d7450ff1f4db1ee77a3357a1bcf2ab4d9b04fa
Sosa Edison <me@sosaedison.io>
2023-01-06 12:08:56 -0600

    update settings for tests

--- app/src/settings.py
+++ app/src/settings.py
@@ -4,9 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Settings(BaseSettings):
-    PLAID_SANDBOX_KEY = os.environ.get("PLAID_SANDBOX_KEY", None)
-    PLAID_CLIENT_ID = os.environ.get("PLAID_CLIENT_ID", None)
-    DB_CONNECTION_STRING = os.environ.get("DB_CONNECTION_STRING", None)
+    PLAID_SANDBOX_KEY = os.environ.get("PLAID_SANDBOX_KEY", "")
+    PLAID_CLIENT_ID = os.environ.get("PLAID_CLIENT_ID", "")
+    DB_CONNECTION_STRING = os.environ.get("DB_CONNECTION_STRING", "sqlite:///:memory")
+
 
 settings = Settings()
