60f2d7d137b7fbfc3149aa136c9156cb07c461ff
Sosa Edison <me@sosaedison.io>
2023-01-06 12:10:28 -0600

    type the settings

--- app/src/settings.py
+++ app/src/settings.py
@@ -6,9 +6,11 @@ load_dotenv()
 
 
 class Settings(BaseSettings):
-    PLAID_SANDBOX_KEY = os.environ.get("PLAID_SANDBOX_KEY", "")
-    PLAID_CLIENT_ID = os.environ.get("PLAID_CLIENT_ID", "")
-    DB_CONNECTION_STRING = os.environ.get("DB_CONNECTION_STRING", "sqlite:///:memory")
+    PLAID_SANDBOX_KEY: str = os.environ.get("PLAID_SANDBOX_KEY", "")
+    PLAID_CLIENT_ID: str = os.environ.get("PLAID_CLIENT_ID", "")
+    DB_CONNECTION_STRING: str = os.environ.get(
+        "DB_CONNECTION_STRING", "sqlite:///:memory"
+    )
 
 
 settings = Settings()
