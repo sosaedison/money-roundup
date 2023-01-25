68688105e2444d65f64c6e606d897c8ab1f62e4c
Sosa Edison <me@sosaedison.io>
2023-01-24 19:13:35 -0600

    better test type checking thanks to linter

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -1,42 +1,41 @@
 from fastapi.testclient import TestClient
-from moneyroundup.schemas import NewUser, LoggedInUser
 
 
 def test_user_registration(client: TestClient):
 
-    new_user: NewUser = {
+    new_user: dict[str, str] = {
         "email": "sosarocks@test.com",
         "first_name": "Sosa",
         "last_name": "Rocks",
         "profile_pic_url": "http://www.some_cool_pic.com",
     }
 
-    res = client.post("/user", json=new_user)
+    client_res = client.post("/user", json=new_user)
 
-    res: LoggedInUser = res.json()
+    res: dict = client_res.json()
 
     assert "user_id" in list(res.keys())
 
 
 def test_user_login(client: TestClient):
 
-    new_user: NewUser = {
+    new_user: dict[str, str] = {
         "email": "sosarocks@test.com",
         "first_name": "Sosa",
         "last_name": "Rocks",
         "profile_pic_url": "http://www.some_cool_pic.com",
     }
 
-    res = client.post("/user", json=new_user)
+    client_res = client.post("/user", json=new_user)
 
-    res: LoggedInUser = res.json()
+    res: dict = client_res.json()
 
     new_user_id = res["user_id"]
 
     assert "user_id" in list(res.keys())  # successful registration
 
-    res = client.post("/user", json=res)
+    client_res = client.post("/user", json=res)
 
-    res = res.json()
+    res = client_res.json()
 
     assert res["user_id"] == new_user_id  # successful login
