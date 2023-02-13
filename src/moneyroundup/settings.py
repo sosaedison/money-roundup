e652dd9c49ddc8585e08737b2d6f15e192e3514c
Sosa Edison <me@sosaedison.io>
2023-02-13 00:28:18 -0600

    add config for jwt token expiration

--- src/moneyroundup/settings.py
+++ src/moneyroundup/settings.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str = os.getenv("SECRET_KEY", "SUPER_SECRET_KEY")
 
     JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM", "HS256")
-    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+    ACCESS_TOKEN_EXPIRE_SECONDS = int(os.getenv("ACCESS_TOKEN_EXPIRE_SECONDS", 30))
 
     PLAID_SANDBOX_KEY: str = os.getenv("PLAID_SANDBOX_KEY", "")
     PLAID_CLIENT_ID: str = os.getenv("PLAID_CLIENT_ID", "")
