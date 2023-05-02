cb8208f7e1703c4fa95fbf1eb1c05bdaa2cf920c
Sosa Edison <me@sosaedison.io>
2023-05-01 23:12:25 -0500

    fix all the tests (#33)

--- src/moneyroundup/fetch_transactions.py
+++ src/moneyroundup/fetch_transactions.py
@@ -2,12 +2,14 @@ import datetime
 
 from plaid.model.transactions_get_request import TransactionsGetRequest
 from plaid.model.transactions_get_request_options import TransactionsGetRequestOptions
-from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from moneyroundup.dependencies import get_db
 from moneyroundup.models import Item, UserOld
 from moneyroundup.plaid_manager import client
 from moneyroundup.rabbit_manager import QueueManager
+from moneyroundup.database import get_async_session_context_manager, User
 
 db = get_db()
 
@@ -38,20 +40,24 @@ def fetch_transactions(access_token: str) -> int:
     return total_transactions
 
 
-def populate_queue_with_transactions(rabbit: QueueManager, session: Session = next(db)):
+async def populate_queue_with_transactions(queue: QueueManager, session = get_async_session_context_manager):
 
-    with session.begin():
-        users: list[UserOld] = session.query(UserOld).all()
+    async with session() as session:
+        # users: list[UserOld] = session.query(UserOld).all()
+        result = await session.execute(select(User))
+        users = [{"id": u.id, "email": u.email} for u in result.scalars()]
 
         for user in users:
 
-            items: list[Item] = (
-                session.query(Item).filter(Item.user_id == user.id).all()
+            items = await (
+                session.execute(select(Item).where(Item.user_id == str(user.get("id"))))
             )
-            access_tokens: list[str] = [str(item.access_token) for item in items]
+            items = [{"user_id": item.user_id, "access_token": item.access_token} for item in items.scalars()]
+
+            access_tokens: list[str] = [str(item.get("access_token")) for item in items]
 
             total_transactions = 0
             for access_token in access_tokens:
                 total_transactions += fetch_transactions(access_token=access_token)
 
-            rabbit.produce(f"The total of your transactions was ${total_transactions}.")
+            queue.produce(f"The total of your transactions was ${total_transactions}.")
--- src/tests/test_token.py
+++ src/tests/test_token.py
@@ -1,59 +1,63 @@
 from unittest.mock import patch
-from uuid import uuid4
 import pytest
+import pytest_asyncio
+from httpx import AsyncClient
+
+from uuid import uuid4
 
 from fastapi.testclient import TestClient
-from pytest import fixture
 
-from moneyroundup.base import Base
-from moneyroundup.database import engine
 from moneyroundup.plaid_manager import client as plaid
+from moneyroundup.database import create_db_and_tables, drop_db_and_tables
 
 
-@fixture(scope="function", autouse=True)
-def rest_db():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
+@pytest_asyncio.fixture(scope="function", autouse=True)
+async def rest_db():
+    """Reset the database before each test."""
+    await drop_db_and_tables()
+    await create_db_and_tables()
 
 
-@pytest.mark.skip(reason="need to update related code to use new database schema")
-def test_create_link_token_for_existing_user(client: TestClient):
+@pytest.mark.asyncio
+async def test_create_link_token_for_existing_user(async_client: AsyncClient):
 
-    new_user = {
-        "email": "sosarocks@test.com",
-        "first_name": "Sosa",
-        "last_name": "Rocks",
-        "profile_pic_url": "http://www.some_cool_pic.com",
-    }
+    # define a user
+    new_user: dict[str, str] = {"email": "sosarocks@test.com", "password": "Sosa"}
 
-    reg_res = client.post("/api/user", json=new_user)
+    # register the user
+    reg_res = await async_client.post("/api/auth/register", json=new_user)
+
+    # assert that the user was created
+    assert reg_res.status_code == 201
+
+    # login with the new user
+    client_res = await async_client.post(
+        "/api/auth/jwt/login",
+        data={"username": new_user["email"], "password": new_user["password"]},
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+    )
+
+    assert client_res.status_code == 200
+
+    # get the access token
+    access_token = client_res.json()["access_token"]
 
     with patch.object(
         plaid,
         "link_token_create",
-        return_value={"link_token": "SIKE"},
+        return_value={"link_token": "McTesty"},
     ):
-        client_res = client.post(
+        client_res = await async_client.post(
             "/api/link/token/create",
-            headers={"Authorization": f"Bearer {reg_res.json()['access_token']}"},
+            headers={"Authorization": f"Bearer {access_token}"},
         )
 
         res: dict = client_res.json()
 
-        assert res["link_token"] == "SIKE"
+        assert res["link_token"] == "McTesty"
 
 
-@pytest.mark.skip(reason="need to update related code to use new database schema")
 def test_create_link_token_for_non_existing_user(client: TestClient):
-
-    new_user = {
-        "email": "sosarocks@test.com",
-        "first_name": "Sosa",
-        "last_name": "Rocks",
-        "profile_pic_url": "http://www.some_cool_pic.com",
-    }
-
-    res = client.post("/user", json=new_user)
 
     with patch.object(
         plaid,
